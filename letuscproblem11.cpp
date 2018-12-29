212d6e962048d0c1aef9594ab7a6fec9b072e490
rahul5720 <46085747+rahul5720@users.noreply.github.com>
2018-12-29 13:40:35 +0530

    Update letuscproblem11.cpp

--- letuscproblem11.cpp
+++ letuscproblem11.cpp
@@ -2,31 +2,25 @@
 /*a positive number is entered  through the keyboard. Write a function to obtain the prime factors of this number.
 for example prime factors of 24 are 2,2,2,3
 */
+int isprime(int n)
+{
+    if (n == 1) return 0;
+	int i = 2;
+	for (; i <= n/2; i++)
+	{
+		if (n % i == 0) return 0;	
+	}
+	return 1;
+}
 int main()
 {
 	int a;
 	printf("Enter a number: ");
 	scanf("%d",&a);
-	for (int i=2;i<=a/2;i++)
+    int i;
+	for (i=2;i<=a;i++)
 	{
-		for (int j=2;j<=i;j++)
-		{
-			if (i%j==0)
-			{
-				break;
-			}
-			if (i==j)
-			{
-				if (a%i==0)
-				{
-					int b;
-					b=a/i;
-					printf("%d %d times, ",i,b);
-				}
-				else
-				    continue;
-			}
-		}
+		if (isprime(i) && (a%i == 0)) printf("%d, ", i);
 	}
-	
+	printf("\b\b \n");
 }
