46e7ffef2903cfad8876cfdea2b05ecce7bc7d1b
aprameya-1234github <42702800+aprameya-1234github@users.noreply.github.com>
2018-12-31 18:53:28 +0530

    Update letuscproblem14.cpp

--- letuscproblem14.cpp
+++ letuscproblem14.cpp
@@ -3,18 +3,21 @@
 #include<math.h>
 int main()
 {
-	int *a,*b;
+	int *a,*b,m;
+	a=(int*)malloc(m*sizeof(int));
+	b=(int*)malloc(m*sizeof(int));
+	scanf("%d",&m);
 	
-	for (int i=0;i<=10;i++)
+	for (int i=0;i<=m;i++)
 	{
-		
-		scanf("%d ",&a[i]);
+		printf("Enter number  ");
+		scanf("%d",a+i);
 	}
-	for (int i=0;i<=10;i++)
+	for (int i=0;i<=m;i++)
 	{
-		a[i]=b[10-i];
+		a[i]=b[m-i];
 	}
-	for (int i=0;i<=10;i++)
+	for (int i=0;i<=m;i++)
 	{
 		printf("%d ",b[i]);
 	}
