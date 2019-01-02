251921f0a8d4b387f4304f2b2c22e61f9e93b7ea
aprameya-1234github <42702800+aprameya-1234github@users.noreply.github.com>
2019-01-02 12:11:13 +0530

    Add files via upload

---
+++ letuscproblem15.cpp
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+int main()
+{
+	int a[25],i,j,key;
+    for (i=0;i<25;i++)
+    {
+    	scanf("%d ",&a[i]);
+	}
+	
+	for(j=1;j<=25;j++)
+	{
+		key = a[j];
+		i=j-1;
+		while(--i>=0&&key<a[i])
+		{
+			a[i+1]=a[i];
+			a[i]=key;
+			
+		}
+		
+	}
+	for(i=0;i<25;i++)
+	{
+		printf("%d ",&a[i]);
+	}
+	return 0;
+}
+
