a153150ce64a409e8d79938fc8602c68e5e5a4c5
rahul5720 <46085747+rahul5720@users.noreply.github.com>
2019-01-04 19:54:49 +0530

    updated interface and used function for simplicity

--- letuscproblem15.cpp
+++ letuscproblem15.cpp
@@ -1,31 +1,40 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<string.h>
+
 /*impleament insertion sort algorithm on a set of 25 numbers*/
-int main()
+
+void insertion_sort(int *arr, int n);
+
+int main(void)
 {
-	int a[25],i,j,key;
-    for (i=0;i<25;i++)
+    int n;
+    printf("Enter the number of elements you will enter : ");
+    scanf("%d", &n);
+    int *arr = (int*)malloc(n * sizeof(int));
+    printf("Enter the elements : ");
+    for (int i = 0; i < n; i++) scanf("%d", &arr[i]);
+    printf("Your entered array is : ");
+    for (int i = 0; i < n; i++) printf("%d ", arr[i]);
+    printf("\n");
+    insertion_sort(arr, n);
+    printf("After sorting the array is : ");
+    for (int i = 0; i < n; i++) printf("%d ", arr[i]);
+    printf("\n");
+    return 0;
+}
+
+void insertion_sort(int *arr, int n)
+{
+    for (int i = 1; i < n; i++)
     {
-    	scanf("%d ",&a[i]);
-	}
-	
-	for(j=1;j<=25;j++)
-	{
-		key = a[j];
-		i=j-1;
-		while(--i>=0&&key<a[i])
-		{
-			a[i+1]=a[i];
-			a[i]=key;
-			
-		}
-		
-	}
-	for(i=0;i<25;i++)
-	{
-		printf("%d ",&a[i]);
-	}
-	return 0;
+        int value = arr[i];
+        int hole = i;
+        while ((hole > 0) && (arr[hole-1] > value))
+        {
+            arr[hole] = arr[hole-1];
+            hole--;
+        }
+        arr[hole] = value;
+    }
 }
 
